f892fea5b9a9fac13036a155edcec812a1fb4fec
Luca Weiss <luca@z3ntu.xyz>
2017-07-28 23:30:39 +0200

    mkinitfs: Add dt.img support

--- aports/postmarketos-mkinitfs/mkinitfs.sh
+++ aports/postmarketos-mkinitfs/mkinitfs.sh
@@ -161,6 +161,7 @@ create_bootimg()
 	if [ -n "${deviceinfo_dtb}" ]; then
 		kernelfile="${kernelfile}-dtb"
 	fi
+	[ -f "${outfile/initramfs-/dt.img-}" ] && dt_img="${outfile/initramfs-/dt.img-}"
 	mkbootimg \
 		--kernel "${kernelfile}" \
 		--ramdisk "$outfile" \
@@ -171,6 +172,7 @@ create_bootimg()
 		--ramdisk_offset "${deviceinfo_flash_offset_ramdisk}" \
 		--tags_offset "${deviceinfo_flash_offset_tags}" \
 		--pagesize "${deviceinfo_flash_pagesize}" \
+		${dt_img:+ --dt "${dt_img}"} \
 		-o "${outfile/initramfs-/boot.img-}"
 }
 
