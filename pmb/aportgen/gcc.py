a4733e94e115d2e3e842fe026c1495cb6c79cf72
Oliver Smith <ollieparanoid@bitmessage.ch>
2019-04-21 21:49:50 +0200

    pmbootstrap aportgen gcc{,4,6}*: fix depends (!1778)
    Explicitly depend on mpc1. Our generated gcc aports use the !tracedeps
    option, so we need to explicitly set the libraries it depends on.
    
    This has mostly not been an issue, as we are installing our gcc
    packages together with Alpine's gcc package, which causes the libraries
    to get installed anyway.
    
    Related: pmaports#236

--- pmb/aportgen/gcc.py
+++ pmb/aportgen/gcc.py
@@ -45,7 +45,7 @@ def generate(args, pkgname):
         "pkgname": pkgname,
         "pkgdesc": "Stage2 cross-compiler for " + arch,
         "arch": "x86_64",
-        "depends": "isl binutils-" + arch,
+        "depends": "isl binutils-" + arch + " mpc1",
         "makedepends_build": "gcc g++ paxmark bison flex texinfo gawk zip gmp-dev mpfr-dev mpc1-dev zlib-dev",
         "makedepends_host": "linux-headers gmp-dev mpfr-dev mpc1-dev isl-dev zlib-dev musl-dev-" + arch + " binutils-" + arch,
         "subpackages": "g++-" + arch + ":gpp" if prefix == "gcc" else "",
