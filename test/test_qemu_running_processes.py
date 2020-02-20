f21c216a26c943a9b625d1091fce65dd2d6450a8
Oliver Smith <ollieparanoid@postmarketos.org>
2020-02-20 21:07:28 +0100

    Cosmetic: use SPDX license header (!1877)
    While at it, also remove unnecessary "#!/usr/bin/env python3" in files
    that only get imported, and adjust other empty/comment lines in the
    beginnings of the files for consistency.
    
    This makes files easier to read, and makes the pmbootstrap codebase more
    consistent with the build.postmarketos.org codebase.

--- test/test_qemu_running_processes.py
+++ test/test_qemu_running_processes.py
@@ -1,22 +1,5 @@
-"""
-Copyright 2020 Oliver Smith
-
-This file is part of pmbootstrap.
-
-pmbootstrap is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-pmbootstrap is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with pmbootstrap.  If not, see <http://www.gnu.org/licenses/>.
-"""
-
+# Copyright 2020 Oliver Smith
+# SPDX-License-Identifier: GPL-3.0-or-later
 """
 This file runs various installations and boots into them with QEMU, then checks
 via SSH if expected processes are running.
@@ -24,7 +7,6 @@ via SSH if expected processes are running.
 We use an extra config file (based on ~/.config/pmbootstrap.cfg), because we
 need to change it a lot (e.g. UI, username, ...).
 """
-
 import os
 import pytest
 import sys
