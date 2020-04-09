b015273d865047b242bb8904e79c07f178ba66c6
Oliver Smith <ollieparanoid@postmarketos.org>
2020-04-09 20:51:39 +0200

    pmb/config/__init__.py: bump pmaports_min_version (MR 1912)
    New pmaports.cfg has the "channel" key described in [1]. This will be
    used in "pmbootstrap init" to determine on which release channel a
    forked branch is.
    
    [1] https://postmarketos.org/pmaports.cfg

--- pmb/config/__init__.py
+++ pmb/config/__init__.py
@@ -24,7 +24,7 @@ apk_keys_path = pmb_src + "/pmb/data/keys"
 apk_tools_static_min_version = "2.10.5-r0"
 
 # postmarketOS aports compatibility (checked against "version" in pmaports.cfg)
-pmaports_min_version = "6"
+pmaports_min_version = "7"
 
 # Version of the work folder (as asked during 'pmbootstrap init'). Increase
 # this number, whenever migration is required and provide the migration code,
