b4a205d4db5c0c543a854fad2bc04af6acfb6232
Oliver Smith <ollieparanoid@postmarketos.org>
2020-06-21 14:28:06 +0200

    pmb/aportgen/busybox_static.py: fix distfile collisions (MR 1949)
    Same fix as MR 1947, but for busybox_static.py instead of musl.py. While
    at it, order the imports alphabetically.

--- pmb/aportgen/busybox_static.py
+++ pmb/aportgen/busybox_static.py
@@ -1,18 +1,15 @@
 # Copyright 2020 Oliver Smith
 # SPDX-License-Identifier: GPL-3.0-or-later
-import glob
-import os
-import pmb.helpers.run
 import pmb.aportgen.core
-import pmb.parse.apkindex
+import pmb.build
 import pmb.chroot.apk
 import pmb.chroot.apk_static
+import pmb.helpers.run
+import pmb.parse.apkindex
 
 
 def generate(args, pkgname):
-    # Install busybox-static in chroot to get verified apks
     arch = pkgname.split("-")[2]
-    pmb.chroot.apk.install(args, ["busybox-static"], "buildroot_" + arch)
 
     # Parse version from APKINDEX
     package_data = pmb.parse.apkindex.package(args, "busybox")
@@ -20,32 +17,17 @@ def generate(args, pkgname):
     pkgver = version.split("-r")[0]
     pkgrel = version.split("-r")[1]
 
-    # Copy the apk file to the distfiles cache
-    pattern = (args.work + "/cache_apk_" + arch + "/busybox-static-" +
-               version + ".*.apk")
-    glob_result = glob.glob(pattern)
-    if not len(glob_result):
-        raise RuntimeError("Could not find aport " + pattern + "!"
-                           " Update your aports_upstream git repo"
-                           " to the latest version, delete your http cache"
-                           " (pmbootstrap zap -hc) and try again.")
-    path = glob_result[0]
-    path_target = (args.work + "/cache_distfiles/busybox-static-" +
-                   version + "-" + arch + ".apk")
-    if not os.path.exists(path_target):
-        pmb.helpers.run.root(args, ["cp", path, path_target])
-
-    # Hash the distfile
-    hashes = pmb.chroot.user(args, ["sha512sum",
-                                    "busybox-static-" + version + "-" + arch + ".apk"],
-                             "buildroot_" + arch, "/var/cache/distfiles",
-                             output_return=True)
+    # Prepare aportgen tempdir inside and outside of chroot
+    tempdir = "/tmp/aportgen"
+    pmb.chroot.root(args, ["rm", "-rf", tempdir])
+    pmb.helpers.run.user(args, ["mkdir", "-p", f"{args.work}/aportgen",
+                                f"{args.work}/chroot_native/{tempdir}"])
 
     # Write the APKBUILD
     channel_cfg = pmb.config.pmaports.read_config_channel(args)
     mirrordir = channel_cfg["mirrordir_alpine"]
-    pmb.helpers.run.user(args, ["mkdir", "-p", args.work + "/aportgen"])
-    with open(args.work + "/aportgen/APKBUILD", "w", encoding="utf-8") as handle:
+    apkbuild_path = f"{args.work}/chroot_native/{tempdir}/APKBUILD"
+    with open(apkbuild_path, "w", encoding="utf-8") as handle:
         apkbuild = f"""\
             # Automatically generated aport, do not edit!
             # Generator: pmbootstrap aportgen {pkgname}
@@ -70,18 +52,21 @@ def generate(args, pkgname):
             _target="$(arch_to_hostspec $_arch)"
 
             source="
-                busybox-static-$pkgver-r$pkgrel-$_arch.apk::$_mirror/{mirrordir}/main/$_arch/busybox-static-$pkgver-r$pkgrel.apk
+                busybox-static-$pkgver-r$pkgrel-$_arch-{mirrordir}.apk::$_mirror/{mirrordir}/main/$_arch/busybox-static-$pkgver-r$pkgrel.apk
             "
 
             package() {{
                 mkdir -p "$pkgdir/usr/$_target"
                 cd "$pkgdir/usr/$_target"
-                tar -xf $srcdir/busybox-static-$pkgver-r$pkgrel-$_arch.apk
+                tar -xf $srcdir/busybox-static-$pkgver-r$pkgrel-$_arch-{mirrordir}.apk
                 rm .PKGINFO .SIGN.*
             }}
         """
         for line in apkbuild.split("\n"):
             handle.write(line[12:].replace(" " * 4, "\t") + "\n")
 
-        # Hashes
-        handle.write("sha512sums=\"" + hashes.rstrip() + "\"\n")
+    # Generate checksums
+    pmb.build.init(args)
+    pmb.chroot.root(args, ["chown", "-R", "pmos:pmos", tempdir])
+    pmb.chroot.user(args, ["abuild", "checksum"], working_dir=tempdir)
+    pmb.helpers.run.user(args, ["cp", apkbuild_path, f"{args.work}/aportgen"])
