8a02187a1a2b48dbcd2a331f32396c303e3c12cb
Oliver Smith <ollieparanoid@postmarketos.org>
2020-06-21 15:06:04 +0200

    pmb/aportgen/grub_efi.py: fix distfile collisions (MR 1950)
    Same fix as MR 1947, but for grub_efi.py instead of musl.py. While at
    it, order the imports alphabetically.

--- pmb/aportgen/grub_efi.py
+++ pmb/aportgen/grub_efi.py
@@ -1,12 +1,11 @@
-# Copyright 2020 Nick Reitemeyer
+# Copyright 2020 Nick Reitemeyer, Oliver Smith
 # SPDX-License-Identifier: GPL-3.0-or-later
-import glob
-import os
-import pmb.helpers.run
 import pmb.aportgen.core
-import pmb.parse.apkindex
+import pmb.build
 import pmb.chroot.apk
 import pmb.chroot.apk_static
+import pmb.helpers.run
+import pmb.parse.apkindex
 
 
 def generate(args, pkgname):
@@ -18,32 +17,17 @@ def generate(args, pkgname):
     pkgver = version.split("-r")[0]
     pkgrel = version.split("-r")[1]
 
-    pmb.chroot.apk.install(args, ["grub-efi"], "buildroot_x86")
-    pattern = (args.work + "/cache_apk_" + arch + "/grub-efi-" +
-               version + ".*.apk")
-    glob_result = glob.glob(pattern)
-    if not len(glob_result):
-        raise RuntimeError("Could not find aport " + pattern + "!"
-                           " Update your aports_upstream git repo"
-                           " to the latest version, delete your http cache"
-                           " (pmbootstrap zap -hc) and try again.")
-    path = glob_result[0]
-    path_target = (args.work + "/cache_distfiles/grub-efi-" +
-                   version + "-" + arch + ".apk")
-    if not os.path.exists(path_target):
-        pmb.helpers.run.root(args, ["cp", path, path_target])
-
-    # Hash the distfile
-    hashes = pmb.chroot.user(args, ["sha512sum",
-                                    "grub-efi-" + version + "-" + arch + ".apk"],
-                             "buildroot_" + arch, "/var/cache/distfiles",
-                             output_return=True)
+    # Prepare aportgen tempdir inside and outside of chroot
+    tempdir = "/tmp/aportgen"
+    pmb.chroot.root(args, ["rm", "-rf", tempdir])
+    pmb.helpers.run.user(args, ["mkdir", "-p", f"{args.work}/aportgen",
+                                f"{args.work}/chroot_native/{tempdir}"])
 
     # Write the APKBUILD
     channel_cfg = pmb.config.pmaports.read_config_channel(args)
     mirrordir = channel_cfg["mirrordir_alpine"]
-    pmb.helpers.run.user(args, ["mkdir", "-p", args.work + "/aportgen"])
-    with open(args.work + "/aportgen/APKBUILD", "w", encoding="utf-8") as handle:
+    apkbuild_path = f"{args.work}/chroot_native/{tempdir}/APKBUILD"
+    with open(apkbuild_path, "w", encoding="utf-8") as handle:
         apkbuild = f"""\
             # Automatically generated aport, do not edit!
             # Generator: pmbootstrap aportgen {pkgname}
@@ -59,16 +43,20 @@ def generate(args, pkgname):
             url="https://www.gnu.org/software/grub/"
             license="GPL-3.0-or-later"
             arch="all"
-            source="grub-efi-$pkgver-r$pkgrel-$_arch.apk::$_mirror/{mirrordir}/main/$_arch/grub-efi-$pkgver-r$pkgrel.apk"
+            source="grub-efi-$pkgver-r$pkgrel-$_arch-{mirrordir}.apk::$_mirror/{mirrordir}/main/$_arch/grub-efi-$pkgver-r$pkgrel.apk"
 
             package() {{
                 mkdir -p "$pkgdir"
                 cd "$pkgdir"
-                tar -xf "$srcdir/grub-efi-$pkgver-r$pkgrel-$_arch.apk"
+                tar -xf "$srcdir/grub-efi-$pkgver-r$pkgrel-$_arch-{mirrordir}.apk"
                 rm .PKGINFO .SIGN.*
             }}
         """
         for line in apkbuild.split("\n"):
             handle.write(line[12:].replace(" " * 4, "\t") + "\n")
 
-        handle.write("sha512sums=\"" + hashes.rstrip() + "\"\n")
+    # Generate checksums
+    pmb.build.init(args)
+    pmb.chroot.root(args, ["chown", "-R", "pmos:pmos", tempdir])
+    pmb.chroot.user(args, ["abuild", "checksum"], working_dir=tempdir)
+    pmb.helpers.run.user(args, ["cp", apkbuild_path, f"{args.work}/aportgen"])
